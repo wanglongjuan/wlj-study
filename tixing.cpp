547f3a578d389c127c47e5996deea329660523c2
Huayi Wei <huayiwei1984@gmail.com>
2016-09-30 17:15:10 +0800

    修改格式

--- tixing.cpp
+++ tixing.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main()
 {
-         float s,h,up,down;
-         up=15;
-         down=25;
-         h=2*150/up;
-         s=(up+down)*h/2;
-         printf("s=%0.2f\n",s);
-         return 0;
+    float s,h,up,down;
+    up=15;
+    down=25;
+    h=2*150/up;
+    s=(up+down)*h/2;
+    printf("s=%0.2f\n",s);
+    return 0;
 }
