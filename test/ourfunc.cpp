e388ef3ed50addb79c67b472ca273e38566267f9
Longjuan Wang <843053935@qq.com>
2016-10-02 21:12:33 +0800

    10-2学习情况10-2学习情况10-2学习情况10-2学习情况10-2学习情况10-2学习情况10-2学习情况10-2学习情况10-2学习情况10-2学习情况

---
+++ test/ourfunc.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+    int x,y;
+    cin >> x;
+    y=12*x;
+    cout <<"Enter your age : "<< y << endl;
+    return 0 ;
+}
---
+++ test/sum1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,s=0;
+    i=1;
+    while(i<=100)
+    {
+        s=s+i;
+        i++;
+    }
+    cout<<"s= " << s <<endl;
+        return 0;
+}
